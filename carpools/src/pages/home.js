1775bc24a1c89ec704a06f10f7eabf123199d596
Abdelhak-Achour <achourabdelhak2001@gmail.com>
2023-12-18 17:49:02 +0100

    made search bar better

--- carpools/src/pages/home.js
+++ carpools/src/pages/home.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/joy";
+import { Autocomplete, Box, Stack } from "@mui/joy";
 import React from "react";
 import carsbanner from "../images/carsbanner.jpg";
 import Card from '@mui/joy/Card';
@@ -11,6 +11,7 @@ import Select from '@mui/joy/Select';
 import Option from '@mui/joy/Option';
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
 import PersonIcon from '@mui/icons-material/Person';
+import data from "../data.json";
 
 export function Home()
 {
@@ -33,42 +34,38 @@ export function Home()
             variant="outlined"
             sx={{
                 maxHeight: 'max-content',
-                maxWidth: '65rem',
+                maxWidth: '61rem',
                 mx: 'auto',
                 marginTop: '1rem'
             }}
             >
-                <CardContent
-                sx={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(5, minmax(80px, 1fr))',
-                    gap: 1.5,
-                }}
-                >
-                    <FormControl>
-                        <Input startDecorator={<LocationSearchingIcon />} placeholder="Leaving from" required />
-                    </FormControl>
-                    <FormControl>
-                        <Input startDecorator={<LocationSearchingIcon />} placeholder="Going to" required />
-                    </FormControl>
-                    <FormControl>
-                        <Input type='date' />
-                    </FormControl>
-                    <Select startDecorator={<PersonIcon />} defaultValue="1" onChange={handleChange}>
-                        <Option value="1">1 passenger</Option>
-                        <Option value="2">2 passengers</Option>
-                        <Option value="3">3 passengers</Option>
-                        <Option value="4">4 passengers</Option>
-                        <Option value="5">5 passengers</Option>
-                        <Option value="6">6 passengers</Option>
-                        <Option value="7">7 passengers</Option>
-                        <Option value="8">8 passengers</Option>
-                    </Select>
-                    <CardActions sx={{padding: "0rem"}}>
-                        <Button variant="solid" sx={{backgroundColor: '#00A9FF', "&:hover": {backgroundColor: '#0099FF'}}}>
-                            Search
-                        </Button>
-                    </CardActions>
+                <CardContent>
+                    <Stack direction={"row"} spacing={2} justifyContent={"center"}>
+                        <FormControl>
+                            <Autocomplete options={data.locations} sx={{maxWidth: "14rem"}} startDecorator={<LocationSearchingIcon />} placeholder="Leaving from" required />
+                        </FormControl>
+                        <FormControl>
+                            <Autocomplete options={data.locations} sx={{maxWidth: "14rem"}} startDecorator={<LocationSearchingIcon />} placeholder="Going to" required />
+                        </FormControl>
+                        <FormControl>
+                            <Input type='date' />
+                        </FormControl>
+                        <Select startDecorator={<PersonIcon />} defaultValue="1" onChange={handleChange}>
+                            <Option value="1">1 passenger</Option>
+                            <Option value="2">2 passengers</Option>
+                            <Option value="3">3 passengers</Option>
+                            <Option value="4">4 passengers</Option>
+                            <Option value="5">5 passengers</Option>
+                            <Option value="6">6 passengers</Option>
+                            <Option value="7">7 passengers</Option>
+                            <Option value="8">8 passengers</Option>
+                        </Select>
+                        <CardActions sx={{padding: "0rem"}}>
+                            <Button variant="solid" sx={{ width: "7rem", backgroundColor: '#00A9FF', "&:hover": {backgroundColor: '#0099FF'}}}>
+                                Search
+                            </Button>
+                        </CardActions>
+                    </Stack>
                 </CardContent>
             </Card>
         </>
